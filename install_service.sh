ed4f221720ec68141b4435d79837f8352f275380
gunu3371 <55932910+gunu3371@users.noreply.github.com>
2024-11-08 23:57:56 +0900

    Update install_service.sh

--- install_service.sh
+++ install_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl disable --now m1s_ups
 apt update
-apt install python3-pip python3-venv
+apt install python3-pip python3-venv python3-dev
 python3 -m pip install -r requirements.txt --break-system-packages
 mkdir -p /etc/m1s_ups/log
 
