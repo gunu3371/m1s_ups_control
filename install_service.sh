5671aceaad9a8c6bffed2593c643caa34382739e
gunu3371 <55932910+gunu3371@users.noreply.github.com>
2025-09-29 11:42:47 +0900

    install_service.sh 업데이트

--- install_service.sh
+++ install_service.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-set -e
 
 systemctl disable --now m1s_ups
-apt update
-apt install python3-pip python3-venv python3-dev
 rm -rf /etc/m1s_ups/
 mkdir /etc/m1s_ups/
 mkdir /var/log/m1s_ups/
+set -e
 
+apt update
+apt install python3-pip python3-venv python3-dev
+cp requirements.txt /etc/m1s_ups/
 cp kill.sh /etc/m1s_ups/
 cp service.py /etc/m1s_ups/
 cp m1s_ups.service /etc/systemd/system/
@@ -15,6 +16,7 @@ cp m1s_ups.service /etc/systemd/system/
 cd /etc/m1s_ups/
 python3 -m venv venv
 venv/bin/python3 -m pip install -r requirements.txt --break-system-packages
+rm /etc/m1s_ups/
 systemctl daemon-reload
 
 systemctl enable --now m1s_ups
